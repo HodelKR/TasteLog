8f2fc9133281a4df6aa0b88cc73182e24d3ae714
JangGyu0 <rjgum123@naver.com>
2023-12-03 17:31:34 +0900

    임시 홈화면 제작 코드 수정

--- app/src/main/java/com/example/tastelog/LeftHomeFragment.java
+++ app/src/main/java/com/example/tastelog/LeftHomeFragment.java
@@ -11,6 +11,7 @@ import android.widget.ImageView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.fragment.app.Fragment;
 
 import com.example.tastelog.databinding.FragmentLefthomeBinding;
@@ -30,38 +31,40 @@ public class LeftHomeFragment extends Fragment{
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
+        CategoryOnClickListener onClickListener = new CategoryOnClickListener();
+
         ImageView book = view.findViewById(R.id.book);
         ImageView music = view.findViewById(R.id.music);
         ImageView tv = view.findViewById(R.id.tv);
 
-        book.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v){
+        book.setOnClickListener(onClickListener);
+        tv.setOnClickListener(onClickListener);
+        music.setOnClickListener(onClickListener);
+    }
+
+    class CategoryOnClickListener implements View.OnClickListener {
+        Intent intent;
+
+        @Override
+        public void onClick(View view) {
+            if(view.getId() == R.id.book) {
                 Log.d("tmp", "book clicked");
-                Intent intent = new Intent(getActivity(), FavoriteActivity.class);
+                intent = new Intent(getActivity(), FavoriteActivity.class);
                 intent.putExtra("category", "book");
                 startActivity(intent);
             }
-        });
-
-        tv.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v){
-                Log.d("tmp", "tv clicked");
-                Intent intent = new Intent(getActivity(), FavoriteActivity.class);
-                intent.putExtra("category", "tv");
-                startActivity(intent);
-            }
-        });
-
-        music.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v){
+            else if(view.getId() == R.id.music) {
                 Log.d("tmp", "music clicked");
-                Intent intent = new Intent(getActivity(), FavoriteActivity.class);
+                intent = new Intent(getActivity(), FavoriteActivity.class);
                 intent.putExtra("category", "music");
                 startActivity(intent);
             }
-        });
+            else if(view.getId() == R.id.tv) {
+                Log.d("tmp", "tv clicked");
+                intent = new Intent(getActivity(), FavoriteActivity.class);
+                intent.putExtra("category", "tv");
+                startActivity(intent);
+            }
+        }
     }
 }
